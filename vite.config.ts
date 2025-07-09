386a002ce22f27ed7994c20cf20036b8c8bfe728
Marcel Gerber <mgerber59@gmail.com>
2025-07-09 17:52:25 +0200

    make into mobx app

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      babel: {
+        parserOpts: {
+          plugins: ["decorators-legacy"], // needed so mobx decorators work correctly
+        },
+      },
+    }),
+  ],
+});
